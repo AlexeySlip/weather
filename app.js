148b824d5ce4b9472565548123c6760556f8582a
Alexey <Aleksey.Slipchenko97@gmail.com>
2021-12-10 17:55:30 +0200

    add Math.round

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 let userDate = new Date();
 let userDay = userDate.getDate();
 let userYear = userDate.getFullYear();
-let userMonth = userDate.getMonth()
+let userMonth = userDate.getMonth();
 
 let time = {
-    hour12: false,
-    minute: "numeric",
-    hour: "numeric",
-    
-  };
-  let newTime = new Intl.DateTimeFormat("en-us", time).format(userDate);
+  hour12: false,
+  minute: "numeric",
+  hour: "numeric",
+};
+let newTime = new Intl.DateTimeFormat("en-us", time).format(userDate);
 
 let dateWeekday = {
-    weekday: "long",
-  };
-  let DayOfWeek = new Intl.DateTimeFormat("en-us", dateWeekday).format(userDate);
+  weekday: "long",
+};
+let DayOfWeek = new Intl.DateTimeFormat("en-us", dateWeekday).format(userDate);
 
 let dateMonth = {
   month: "short",
@@ -22,24 +21,24 @@ let dateMonth = {
 let month = new Intl.DateTimeFormat("en-us", dateMonth).format(userDate);
 
 class Weather {
-    constructor( data, parent){
-        this.name = data.name,
-        this.humidity = data.main.humidity,
-        this.pressure = data.main.pressure,
-        this.wind = data.wind.speed,
-        this.icon = data.weather[0]["icon"],
-        this.temp = data.main.temp,
-        this.feels_like = data.main.feels_like,
-        this.deg = data.deg,
-        this.description = data.weather[0]["description"],
-        this.parent = this.parent = document.querySelector(".weather");
-    }
+  constructor(data, parent) {
+    (this.name = data.name),
+      (this.humidity = data.main.humidity),
+      (this.pressure = data.main.pressure),
+      (this.wind = data.wind.speed),
+      (this.icon = data.weather[0]["icon"]),
+      (this.temp = Math.round(data.main.temp)),
+      (this.feels_like = Math.round(data.main.feels_like)),
+      (this.deg = data.deg),
+      (this.description = data.weather[0]["description"]),
+      (this.parent = this.parent = document.querySelector(".weather"));
+  }
 
-    render() {
-        let article = document.createElement("article");
-        article.classList.add("row");
-        article.classList.add("weather-bacgraund");
-        article.innerHTML = `
+  render() {
+    let article = document.createElement("article");
+    article.classList.add("row");
+    article.classList.add("weather-bacgraund");
+    article.innerHTML = `
           <div class="weather-stats-1">
             <div class="date">${userDay} ${month} ${userYear}</div>
             <div class="DayOfWeek">${DayOfWeek}</div>
@@ -56,13 +55,15 @@ class Weather {
             <div class="description">${this.description}</div>
         </div>
         `;
-        this.parent.append(article);
-      }
+    this.parent.append(article);
+  }
 }
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=KHARKIV&units=metric&APPID=5d066958a60d315387d9492393935c19&")
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=KHARKIV&units=metric&APPID=5d066958a60d315387d9492393935c19&"
+)
   .then((response) => response.json())
-  .then((data) => new Weather( data, parent).render());
+  .then((data) => new Weather(data, parent).render());
 
 let input = document.querySelector(".new-city-input");
 let buttonWeather = document.querySelector(".new-city-btn");
